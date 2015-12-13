865a300833a4f3923cf80c12b99bf7006bda0c2f
Julien Coutault <julien.coutault@orange.fr>
2015-12-13 15:27:08 +0100

    Pied de page

---
+++ web/footer.php
@@ -0,0 +1,13 @@
+</div><!-- Container-->
+
+<footer>
+
+<script type="text/javascript" scr="/ppe_pharmadrive_noob/web/js/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" scr="/ppe_pharmadrive_noob/web/js/modal.js"></script>
+
+<script type="text/javascript" src="/ppe_pharmadrive_noob/web/js/bootstrap.js"></script>
+
+</footer>
+</body>
+</html>
