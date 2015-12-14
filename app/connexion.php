4ecf2f9473d10dea80f0d6887c0e5f2e1542aa0a
Julien Coutault <julien.coutault@orange.fr>
2015-12-14 22:14:40 +0100

    Create connexion.php

---
+++ app/connexion.php
@@ -0,0 +1,44 @@
+<?php 
+include_once 'demarage.php';
+
+$erreur = false;
+
+if (isset($_POST['mail']) && empty($_POST['mail']))
+{
+	$erreur = true;
+}
+else if (isset($_POST['pass']) && empty($_POST['pass']))
+{
+	$erreur = true;
+}
+
+if($erreur)
+{
+	$_SESSION['alert'] = new Alert('danger','Erreur : le couple utilisateur/mot de passe incorect');
+	redirige("../web/index");
+}
+else
+{
+	$user = new User();
+	$user->setLogin($_POST['mail']);
+	$user->setPass($_POST['pass']);
+
+	
+
+	if($userDao->connexion($user))
+	{
+		$user = $userDao->hydrate($user);
+		$user->setAuthentified(true);
+		$_SESSION['user']= $user;
+
+		$_SESSION['alert'] = new Alert('success','Bonjour '.$user->getLogin());
+		redirige("../web/index");
+	}
+	else
+	{
+		$_SESSION['alert'] = new Alert('danger','Erreur : le couple utilisateur/mot de passe incorect');
+		redirige("../web/index");
+	}
+}
+
+?>
