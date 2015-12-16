3da20d38424da01b96cee001c115c097ea6fbd50
Julien Coutault <julien.coutault@orange.fr>
2015-12-16 21:46:45 +0100

    Allegement
    - Plus d'inclusion de demarage.php (role du controleur)
    - Plus de vérification des champs (role du controleur)
    - Plus de redirection (encore le role du controleur)

--- app/connexion.php
+++ app/connexion.php
@@ -1,42 +1,19 @@
 <?php 
-include_once 'demarage.php';
 
-$erreur = false;
+$user = new User();
+$user->setLogin($form['login']);
+$user->setPass($form['pass']);
 
-if (isset($_POST['mail']) && empty($_POST['mail']))
+if($userDao->connexion($user))
 {
-	$erreur = true;
-}
-else if (isset($_POST['pass']) && empty($_POST['pass']))
-{
-	$erreur = true;
-}
-
-if($erreur)
-{
-	$_SESSION['alert'] = new Alert('danger','Erreur : le couple utilisateur/mot de passe incorect');
-	redirige("index");
+	$user = $userDao->hydrate($user);
+	$user->setAuthentified(true);
+	$_SESSION['user']= $user;
+	$_SESSION['alert'] = new Alert('success','Bonjour '.$user->getLogin());
 }
 else
 {
-	$user = new User();
-	$user->setLogin($_POST['mail']);
-	$user->setPass($_POST['pass']);
-
-	if($userDao->connexion($user))
-	{
-		$user = $userDao->hydrate($user);
-		$user->setAuthentified(true);
-		$_SESSION['user']= $user;
-
-		$_SESSION['alert'] = new Alert('success','Bonjour '.$user->getLogin());
-		redirige("index");
-	}
-	else
-	{
-		$_SESSION['alert'] = new Alert('danger','Erreur : le couple utilisateur/mot de passe incorect');
-		redirige("index");
-	}
+	$_SESSION['alert'] = new Alert('danger','Erreur : le couple utilisateur/mot de passe incorect');
 }
 
-?>
+
