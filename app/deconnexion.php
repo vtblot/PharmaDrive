3a55a01340ccb78b1f8764a4f050d371adf689da
Julien Coutault <julien.coutault@orange.fr>
2015-12-16 21:44:50 +0100

    Allegement
    - plus la peine d'inclure demarage.php (le controleur s'en charge)
    - Suppression de la redirection (le controleur s'en charge aussi)

--- app/deconnexion.php
+++ app/deconnexion.php
@@ -1,9 +1,5 @@
 <?php 
-	include_once 'demarage.php';
 
 	session_unset();
-	//session_destroy();
 
 	$_SESSION['alert'] = new Alert('success','Vous avez bien été déconnecté');
-
-	redirige('index');
