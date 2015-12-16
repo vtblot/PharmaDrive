f9fb6e7dc61e400ce038aca48b58fe4d38640503
Julien Coutault <julien.coutault@orange.fr>
2015-12-16 16:21:30 +0100

    Create listeVisite.php
    Affiche les visites

---
+++ web/visite/listeVisite.php
@@ -0,0 +1,22 @@
+<table class="table table-striped table-hover ">
+  <thead>
+    <tr class="head">
+    	<th></th>
+     	<th>Patient</th>
+      	<th>Date</th>
+      	<th>Commentaire</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <?php foreach ($visites as $visite): ?>
+    <tr class="lien info">
+      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$i++?></td>
+      	<td onclick="document.location='/ppe_pharmadrive_noob/patients/<?=$visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom()?>'"><?=$visite->getPatient()->getNom().' '.$visite->getPatient()->getPrenom()?></td>
+      	<td><?=$visite->getDateVisite()?></td>
+      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$visite->getCommentaire()?></td>
+    </tr>
+    <?php endforeach ?>
+   
+  </tbody>
+</table> 
