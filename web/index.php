3bd671b926e6f899507da7e6c39f177ffd52b7b9
Julien Coutault <julien.coutault@orange.fr>
2015-12-16 21:51:06 +0100

    Transformation en controleur
    Gère les url : principale (/pharmadrive)
                        - connexion
                        - deconnecion

--- web/index.php
+++ web/index.php
@@ -1,43 +1,74 @@
 <?php
 
+require_once '../app/demarage.php';
+
 include_once 'nav.php';
 
-$form = new Form(array('name' => 'Julien' ));
-/*
-echo $form->input("name");
-echo $form->input("mdp");
-echo $form->subit();*/
 
-if(!$co)
+if(isset($_GET['lien']))
 {
-	echo '<form class="form-signin" method="post" action="../app/connexion.php">';
-	echo '<fieldset>';
-	echo '<h2 class="form-signin-heading">Connexion</h2>';
-	echo '<label for="mail" class="sr-only">Login</label>';
-	echo '<input type="text" id="mail" name="mail" class="form-control" placeholder="Nom d\'utilisateur" autofocus>';
-	echo '<label for="pass" class="sr-only">Mot de passe</label>';
-	echo '<input type="password" id="pass" name="pass" class="form-control" placeholder="Mot de passe" >';
-	echo '</fieldset>';
-	echo '<input class="btn btn-lg btn-primary btn-block" type="submit" value="Connexion"/>';
-	echo '</form>';
+	switch ($_GET['lien']) {
+		case 'deconnexion':
+			if(!$co)
+			{
+				//si il est pas co, on le renvoie
+				redirige('/ppe_pharmadrive_noob');
+			}
+			//on inclu la page de deconnexion
+			include_once '../app/deconnexion.php';
+			//et on redirige
+			redirige('/ppe_pharmadrive_noob');
+			break;
+		case 'connexion':
+			if($co)
+			{
+				//si il est co, on le renvoie
+				redirige('/ppe_pharmadrive_noob');
+			}
+			//on vérifie la présence des chaines de caractère
+			if (isset($_SESSION['login']) && empty($_SESSION['login']))
+			{
+				$_SESSION['alert'] = new Alert('danger','Erreur : le nom d\'utilisateur n\'a pas été renseillé');
+			}
+			else if (isset($_SESSION['pass']) && empty($_SESSION['pass']))
+			{
+				$_SESSION['alert'] = new Alert('danger','Erreur : le mot de passe n\'a pas été renseillé');
+			}
+			else
+			{
+				//tout est ok
+				$form = array('login' => $_POST['login'], 'pass' => $_POST['pass']);
+				include_once '../app/connexion.php';
+			}
+			redirige('/ppe_pharmadrive_noob');
+			break;
+		default:
+			//Si on est a la 'racine'
+			if($co)
+			{
+				echo '<ul>';
+					echo '<li>Patients : reste a faire la page du patient</li>';
+					echo '<li>Visites : reste les pages uniques</li>';
+				echo '</ul>';
+			}
+			else
+			{
+				echo '<form class="form-signin" method="post" action="connexion">';
+				echo '<fieldset>';
+				echo '<h2 class="form-signin-heading">Connexion</h2>';
+				echo '<label for="login" class="sr-only">Login</label>';
+				echo '<input type="text" id="mail" name="login" class="form-control" placeholder="Nom d\'utilisateur" autofocus>';
+				echo '<label for="pass" class="sr-only">Mot de passe</label>';
+				echo '<input type="password" id="pass" name="pass" class="form-control" placeholder="Mot de passe" >';
+				echo '</fieldset>';
+				echo '<input class="btn btn-lg btn-primary btn-block" type="submit" value="Connexion"/>';
+				echo '</form>';
+			}
+			include_once 'connexion.php';
+			break;
+	}
 }
-else
-{
-	?>
-	<ul>
-		<li>Patients : reste a faire la page du patient</li>
-		<li>Visites : reste les pages uniques</li>
-	</ul>
 
-	<?php 
-}
-?>
-
-
-
-
-
-<?php 
 
 include_once 'footer.php';
 ?>
