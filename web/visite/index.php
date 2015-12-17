bbdc6881ee81fec69a61162491a92af4da44ce27
Julien Coutault <julien.coutault@orange.fr>
2015-12-17 22:08:20 +0100

    Redefinition du controleur
    - demarage.php devient une nécessité (require)
    - mise en conformité avec le controleur de l'index (avec $_GET['lien'])

--- web/visite/index.php
+++ web/visite/index.php
@@ -1,23 +1,53 @@
 <?php 
 
-include_once '../nav.php';
+require_once '../../app/demarage.php';
 
 
 if(!$medoc)
 {
 	//si il est pas medecin, on le renvoie à l'aceuil
-	redirige('index');
+	redirige('/ppe_pharmadrive_noob');
 }
 
 
 $i = 1;
 
-if(isset($_GET['nom']) and isset($_GET['prenom']))
-{
-  	//on a renseillé un patient précis
+include_once '../nav.php';
 
+
+var_dump($_GET);
+
+if(isset($_GET['lien']))
+{
+	switch ($_GET['lien']) {
+		case 'detail':
+			//on veut voir le detail d'une visite
+			
+			if(!isset($_GET['id']) && is_int($_GET['id']))
+			{
+			  	//si on a un id invalide de visite
+
+				//on créer un message d'erreur et on redirige
+				$_SESSION['alert'] = new Alert('erreur','Visite introuvable');
+				redirige('/ppe_pharmadrive_noob/visite');
+			}
+			/**
+			*
+			*  METTRE LE CODE CORRESPONDANT
+			*
+			*/
+			break;
+		case 'nouveau':
+			//on veut créer une nouvelle visite
+			include_once 'nouveau.php';
+			break;
+		default:
+			//en cas de lien invalide, on redirige vers l'index
+			redirige('/ppe_pharmadrive_noob/visite');
+			break;
+	}
 }
-else
+else 
 {
   	//sinon on liste les visites du médecin
 
