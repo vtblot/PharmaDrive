5b5457a690a4f62fd2365e24b198fd1f3a69187c
Julien Coutault <julien.coutault@orange.fr>
2015-12-18 17:03:56 +0100

    Transformation en controleur
    Controformation en controleur sur l'image de l'index (prend les liens à travert $_GET['lien'] )

--- web/patient/index.php
+++ web/patient/index.php
@@ -1,27 +1,63 @@
 <?php
 
+require_once '../../app/demarage.php';
+
 include_once '../nav.php';
+
 
 if(!$medoc)
 {
-  //si il est pas medecin, on le renvoie à l'aceuil
-  redirige('index');
+  	//si il est pas medecin, on le renvoie à l'aceuil
+  	redirige('../');
 }
 
-$patients = $patientDao->selectAll();
 $i = 1;
 
 var_dump($_GET);
 
-if(isset($_GET['id']))
+if(isset($_GET['lien']))
 {
-  //on a renseillé un patient précis
+	switch ($_GET['lien']) {
+		case 'detail':
+			//on veut voir le detail d'une patient
+			
+			if(!isset($_GET['nom']) && is_int($_GET['prenom']))
+			{
+			  	//si on a pas renseiller le nom et le prenom pour le patient
 
+				//on créer un message d'erreur et on redirige
+				$_SESSION['alert'] = new Alert('erreur','Patient introuvable');
+				redirige('/ppe_pharmadrive_noob/patient');
+			}
+
+			/*
+			* Verifier que le patient existe
+			*/
+
+
+			/**
+			*
+			*  METTRE LE CODE CORRESPONDANT
+			*
+			*/
+			break;
+		case 'nouveau':
+			//on veut créer un patient
+			include_once 'nouveau.php';
+			break;
+		default:
+			//en cas de lien invalide, on redirige vers l'index
+			redirige('/ppe_pharmadrive_noob/patient');
+			break;
+	}
 }
-else
+else 
 {
-  //sinon on liste les visites du médecin
-  include_once 'listePatient.php';
+  	//sinon on liste les visites du médecin
+
+	//on recupere les visites du docteur
+	$patients = $patientDao->selectAll();
+  	include_once 'listePatient.php';
 }
 
 
