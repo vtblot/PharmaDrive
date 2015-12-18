c0a12cfc32e27f8dc5e37af9ac1f5d2fb3c61d29
Julien Coutault <julien.coutault@orange.fr>
2015-12-18 14:37:29 +0100

    Corection minime
    - correction du lien patient
    - ajout d'un bouton pour créer une visite

--- web/visite/listeVisite.php
+++ web/visite/listeVisite.php
@@ -1,3 +1,5 @@
+<a class="btn btn-primary float" href="visite/nouveau">Ajouter une visite</a>
+
 <table class="table table-striped table-hover ">
   <thead>
     <tr class="head">
@@ -12,7 +14,7 @@
     <?php foreach ($visites as $visite): ?>
     <tr class="lien info">
       	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$i++?></td>
-      	<td onclick="document.location='/ppe_pharmadrive_noob/patients/<?=$visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom()?>'"><?=$visite->getPatient()->getNom().' '.$visite->getPatient()->getPrenom()?></td>
+      	<td onclick="document.location='/ppe_pharmadrive_noob/patient/<?=$visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom()?>'"><?=$visite->getPatient()->getNom().' '.$visite->getPatient()->getPrenom()?></td>
       	<td><?=$visite->getDateVisite()?></td>
       	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$visite->getCommentaire()?></td>
     </tr>
