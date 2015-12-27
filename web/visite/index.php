950aa56c955ec50f882c2f0d1bbd7b6fb5d926e1
Julien Coutault <julien.coutault@orange.fr>
2015-12-27 21:41:36 +0100

    Update index.php
    - Modification de la condition pour savoir si on a bien donner un int pour le détail
    - Ajout d'une condition pour savoir si la visite est bien dans la base de donnée
    - Sélection des patients pour créer une nouvelle visites
    - Ajout de message d'erreur si la page demandée dans visite/ n'existe pas

--- web/visite/index.php
+++ web/visite/index.php
@@ -11,7 +11,6 @@ if(!$medoc)
 
 
 $i = 1;
-
 include_once '../nav.php';
 
 
@@ -23,14 +22,23 @@ if(isset($_GET['lien']))
 		case 'detail':
 			//on veut voir le detail d'une visite
 			
-			if(!isset($_GET['id']) && is_int($_GET['id']))
+			if(!isset($_GET['id']) && !is_int($_GET['id']))
 			{
 			  	//si on a un id invalide de visite
 
 				//on créer un message d'erreur et on redirige
+				$_SESSION['alert'] = new Alert('erreur','Visite invalide');
+				redirige('/ppe_pharmadrive_noob/visite');
+			}
+			else if(!$visiteDao->exist($_GET['id']))
+			{
+				//la visite n'est pas dans la bdd
+
 				$_SESSION['alert'] = new Alert('erreur','Visite introuvable');
 				redirige('/ppe_pharmadrive_noob/visite');
 			}
+
+			var_dump($visiteDao->select($_GET['id']));
 			/**
 			*
 			*  METTRE LE CODE CORRESPONDANT
@@ -39,10 +47,15 @@ if(isset($_GET['lien']))
 			break;
 		case 'nouveau':
 			//on veut créer une nouvelle visite
+
+			//on a besion de la liste des pastients
+			$patients = $patientDao->selectAll();
 			include_once 'nouveau.php';
 			break;
 		default:
 			//en cas de lien invalide, on redirige vers l'index
+
+			$_SESSION['alert'] = new Alert('erreur','Cette page n\'existe pas');
 			redirige('/ppe_pharmadrive_noob/visite');
 			break;
 	}
