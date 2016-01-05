07711e48cb20afeda079baa3eaacff5e6c3c1d92
Julien Coutault <julien.coutault@orange.fr>
2016-01-05 15:19:06 +0100

    Ajout de la fonction mailcorect()
    Vérifie que la chaine passée en paramètre correspond à une adresse email

--- app/fonction.php
+++ app/fonction.php
@@ -1,6 +1,5 @@
 <?php
-
-
+	
 	function getLocalHeure()
 	{
 		$heure = date('H').':'.$minute = date('i');
@@ -13,10 +12,21 @@
 		return $jour;
 	}
 	
+	function mailCorect($email)
+	{
+		if(preg_match("/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/", $email))
+  			return true;
+		else
+			return false;
+	}
+
 	function redirige($lien = 'index')
 	{
-		header('Location:'.$lien);
-		exit;
+		if(isset($lien))
+		{
+			header('Location:'.$lien);
+			exit;
+		}
 	}
 	
 ?>
