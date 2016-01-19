73981167a6d9c768d4ddc80a82127d03a1befafe
Julien Coutault <julien.coutault@orange.fr>
2016-01-19 16:38:42 +0100

    Ajout fonction chaine_aleatoire

--- app/fonction.php
+++ app/fonction.php
@@ -1,4 +1,16 @@
 <?php
+	function chaine_aleatoire($nb_car, $chaine = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN123456789')
+	{
+	    $nb_lettres = strlen($chaine) - 1;
+	    $generation = '';
+	    for($i=0; $i < $nb_car; $i++)
+	    {
+	        $pos = mt_rand(0, $nb_lettres);
+	        $car = $chaine[$pos];
+	        $generation .= $car;
+	    }
+	    return $generation;
+	}
 	
 	function getLocalHeure()
 	{
