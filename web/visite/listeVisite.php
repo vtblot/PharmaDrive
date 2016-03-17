9014c3918a1fa5865d92a0bd2b8b170a9730c43a
Julien Coutault <julien.coutault@orange.fr>
2016-03-17 10:04:33 +0100

    Update listeVisite.php

--- web/visite/listeVisite.php
+++ web/visite/listeVisite.php
@@ -1,6 +1,6 @@
 <a class="btn btn-primary float" href="visite/nouveau">Ajouter une visite</a>
 
-<table class="table table-striped table-hover ">
+<table class="table table-striped table-hover tableau" >
   <thead>
     <tr class="head">
     	<th></th>
@@ -13,10 +13,11 @@
 
     <?php foreach ($visites as $visite): ?>
     <tr class="lien info">
-      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$i++?></td>
-      	<td onclick="document.location='/ppe_pharmadrive_noob/patient/<?=$visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom()?>'"><?=$visite->getPatient()->getNom().' '.$visite->getPatient()->getPrenom()?></td>
-      	<td><?=$visite->getDateVisite()?></td>
-      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?=$visite->getId()?>'"><?=$visite->getCommentaire()?></td>
+        <!-- On met des liens si jamais l'utilisateur a désactiver le javascript -->
+      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?php echo $visite->getId(); ?>'"><?php echo $i++; ?></td>
+      	<td onclick="document.location='/ppe_pharmadrive_noob/patient/<?php echo $visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom(); ?>'"><?php echo $visite->getPatient()->getNom(),' ',$visite->getPatient()->getPrenom(); ?></td>
+      	<td><?php echo $visite->getDateVisite(); ?></td>
+      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?php echo $visite->getId(); ?>'"><?php echo $visite->getCommentaire(); ?></td>
     </tr>
     <?php endforeach ?>
    
