52ca973676453f6dd28cd35de816ebd66bb50133
Julien Coutault <julien.coutault@orange.fr>
2016-03-20 19:14:43 +0100

    Create detail.php

---
+++ web/ordonnance/detail.php
@@ -0,0 +1,64 @@
+
+<div class="jumbotron">
+	<form class="form-horizontal">
+
+		<fieldset>
+			<legend>Ordonnance du <?php echo $ordonnance->getJour()->format('d-m-Y'); ?></legend>
+			<table style="width:100%" cellspacing="10">
+				<tr>
+					<td style="width:42%">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">Praticien</h3>
+							</div>
+							<div class="panel-body">
+								<blockquote>
+									<p><?php 
+									echo "Prénom : ".$ordonnance->getVisite()->getMedecin()->getPrenom()." <br/>"."<br/>";
+									echo "Nom : ".$ordonnance->getVisite()->getMedecin()->getNom(); ?></p>
+									<small><?php echo "Fonction : ".ucfirst($ordonnance->getVisite()->getMedecin()->getFonction()); ?></small>
+								</blockquote>
+							</div>
+						</div>
+					</td>
+					<td style="width:5%"></td>
+					<td style="width:43%">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">Patient</h3>
+							</div>
+							<div class="panel-body">
+								<blockquote>
+									<p><?php 
+									echo "Prénom : ".$ordonnance->getVisite()->getPatient()->getPrenom()." <br/>"."<br/>";
+									echo "Nom : ".$ordonnance->getVisite()->getPatient()->getNom(); ?></p>
+									<small><?php echo "NumSécu : ".$ordonnance->getVisite()->getPatient()->getNumSecu(); ?></small>
+								</blockquote>
+							</div>
+						</div>
+					</td>
+				</tr>
+			</table>
+
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">Récapitulatif</h3>
+
+				</div>
+				<div class="panel-body">
+					<strong>
+					<?php 
+					echo "<p>Médicament : ", $ordonnance->getMedicament()->getNom(),'</p>';
+					echo "<p>Boites : ", $ordonnance->getQte(),'</p>';
+					echo "<p>Observations : ", $ordonnance->getCommentaire(),'</p>'; 
+					?>
+					</strong>
+				</div>
+			</div>
+
+
+
+		</fieldset>
+	</form>
+</div>
+
