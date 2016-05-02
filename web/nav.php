51480429a7be02c68a5683f1b90ca5e06508b2db
Julien Coutault <julien.coutault@orange.fr>
2016-05-02 17:58:41 +0200

    Update nav.php

--- web/nav.php
+++ web/nav.php
@@ -17,27 +17,16 @@ else
 	<meta charset="utf-8" />
 	<title><?=$titre?></title>
 
-	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive_noob/web/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive_noob/web/css/connexion.css">
-	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive_noob/web/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive/web/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive/web/css/connexion.css">
+	<link rel="stylesheet" type="text/css" href="/ppe_pharmadrive/web/css/style.css">
 </head>
 
 <body>
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="/ppe_pharmadrive_noob/">Pharmadrive</a>
-			<?php if($co)
-			{
-				//si l'utilisateur est connecté
-				echo '<form class="navbar-form navbar-left" role="search">'.PHP_EOL;
-        		echo '	<div class="form-group">'.PHP_EOL;
-          		echo '		<input class="form-control" type="text">'.PHP_EOL;
-        		echo '	</div>'.PHP_EOL;
-        		echo '	<button type="submit" class="btn btn-primary">Rechercher</button>'.PHP_EOL;
-     			echo '</form>'.PHP_EOL;
-			} ?>
-			
+			<a class="navbar-brand" href="/ppe_pharmadrive/">Pharmadrive</a>			
 		</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
@@ -48,15 +37,16 @@ else
 					if($medecin)
 					{
 						//si l'utilisateur est un médecin
-						echo '<li><a href="/ppe_pharmadrive_noob/ordonnance">Ordonnances</a></li>'.PHP_EOL;
-						echo '<li><a href="/ppe_pharmadrive_noob/visite">Visites</a></li>'.PHP_EOL;
+						echo '<li><a href="/ppe_pharmadrive/patient">Patients</a></li>'.PHP_EOL;
+						echo '<li><a href="/ppe_pharmadrive/ordonnance">Ordonnances</a></li>'.PHP_EOL;
+						echo '<li><a href="/ppe_pharmadrive/visite">Visites</a></li>'.PHP_EOL;
 					}
 					else if($pharmacien)
 					{
 						//si l'utilisateur est un pharmacien
-						echo '<li><a href="/ppe_pharmadrive_noob/comamnde">Commandes</a></li>'.PHP_EOL;
+						echo '<li><a href="/ppe_pharmadrive/ordonnance">Ordonnances</a></li>'.PHP_EOL;
 					}
-					echo '<li><a href="/ppe_pharmadrive_noob/deconnexion">Déconnexion</a></li>'.PHP_EOL;
+					echo '<li><a href="/ppe_pharmadrive/deconnexion">Déconnexion</a></li>'.PHP_EOL;
 				}
 				?>
 			</ul>
