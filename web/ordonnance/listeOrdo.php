f3e855127dcc7a88e4308f5b11bed86f2a5ad0b2
Julien Coutault <julien.coutault@orange.fr>
2016-05-02 18:06:46 +0200

    Update listeOrdo.php

--- web/ordonnance/listeOrdo.php
+++ web/ordonnance/listeOrdo.php
@@ -5,17 +5,36 @@
       <th>Date</th>
       <th>Medicament</th>
       <th>Nombre de boite</th>
+      <?php
+          if($pharmacien)
+          {
+            echo '<th>Délivré par</th>';
+          }
+        ?>
     </tr>
   </thead>
   <tbody>
 
   	<?php foreach ($ordonnances as $ordonnance): ?>
-  	<tr class="lien <?php echo ($ordonnance->getFini()==0)?'danger':'success' ?>" onclick="document.location='/ppe_pharmadrive_noob/ordonnance/<?php echo $ordonnance->getId(); ?>'">
+  	<tr class="lien <?php echo ($ordonnance->estFinie())?'success':'danger' ?>" onclick="document.location='/ppe_pharmadrive/ordonnance/<?php echo $ordonnance->getId(); ?>'">
       	<td><?php echo $i++; ?></td>
         <!-- On met des liens si jamais l'utilisateur a désactiver le javascript -->
-      	<td><a href="/ppe_pharmadrive_noob/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getJour()->format('d-m-Y'); ?></a></td>
-      	<td><a href="/ppe_pharmadrive_noob/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getMedicament()->getNom(); ?></a></td>
-      	<td><a href="/ppe_pharmadrive_noob/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getQte(); ?></a></td>
+      	<td><a href="/ppe_pharmadrive/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getJour()->format('d-m-Y'); ?></a></td>
+      	<td><a href="/ppe_pharmadrive/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getMedicament()->getNom(); ?></a></td>
+      	<td><a href="/ppe_pharmadrive/ordonnance/<?php echo $ordonnance->getId(); ?>"><?php echo $ordonnance->getQte(); ?></a></td>
+        <?php
+          if($pharmacien)
+          {
+            if($ordonnance->estFinie())
+            {
+              $pharma = $userDao->select($ordonnance->getIdPharmacien());
+              echo '<td>',$pharma->getNom(),' ',$pharma->getPrenom(),'</a></td>';
+            }
+            else{
+              echo '<td></a></td>';
+            }
+          }
+        ?>
     </tr>
   	<?php endforeach ?>
     
