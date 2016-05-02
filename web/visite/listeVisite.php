28bbea94db16eda82220a860b805b5b3258671f5
Julien Coutault <julien.coutault@orange.fr>
2016-05-02 18:09:26 +0200

    Update listeVisite.php

--- web/visite/listeVisite.php
+++ web/visite/listeVisite.php
@@ -1,6 +1,7 @@
-<a class="btn btn-primary float" href="visite/nouveau">Ajouter une visite</a>
-
-<table class="table table-striped table-hover tableau" >
+<?php
+  $i = 1;
+?>
+<table class="table table-striped table-hover tableau">
   <thead>
     <tr class="head">
     	<th></th>
@@ -14,10 +15,10 @@
     <?php foreach ($visites as $visite): ?>
     <tr class="lien info">
         <!-- On met des liens si jamais l'utilisateur a désactiver le javascript -->
-      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?php echo $visite->getId(); ?>'"><?php echo $i++; ?></td>
-      	<td onclick="document.location='/ppe_pharmadrive_noob/patient/<?php echo $visite->getPatient()->getNom().'_'.$visite->getPatient()->getPrenom(); ?>'"><?php echo $visite->getPatient()->getNom(),' ',$visite->getPatient()->getPrenom(); ?></td>
+      	<td onclick="document.location='/ppe_pharmadrive/visite/<?php echo $visite->getId(); ?>'"><?php echo $i++; ?></td>
+      	<td onclick="document.location='/ppe_pharmadrive/patient/<?php echo $visite->getPatient()->getId(),'/',$visite->getPatient()->getNom(),'_',$visite->getPatient()->getPrenom(); ?>'"><?php echo $visite->getPatient()->getNom(),' ',$visite->getPatient()->getPrenom(); ?></td>
       	<td><?php echo $visite->getDateVisite(); ?></td>
-      	<td onclick="document.location='/ppe_pharmadrive_noob/visite/<?php echo $visite->getId(); ?>'"><?php echo $visite->getCommentaire(); ?></td>
+      	<td onclick="document.location='/ppe_pharmadrive/visite/<?php echo $visite->getId(); ?>'"><?php echo $visite->getCommentaire(); ?></td>
     </tr>
     <?php endforeach ?>
    
