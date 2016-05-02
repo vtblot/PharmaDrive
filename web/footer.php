bb98cce81379bc27bf037fc3c0c936bc081190d9
Julien Coutault <julien.coutault@orange.fr>
2016-05-02 17:58:13 +0200

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -2,11 +2,13 @@
 
 <footer>
 
-<script type="text/javascript" scr="/ppe_pharmadrive_noob/web/js/jquery.js"></script>
+<script type="text/javascript" scr="/ppe_pharmadrive/web/js/jquery.js"></script>
+<!--
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script type="text/javascript" scr="/ppe_pharmadrive_noob/web/js/modal.js"></script>
+-->
+<script type="text/javascript" scr="/ppe_pharmadrive/web/js/modal.js"></script>
 
-<script type="text/javascript" src="/ppe_pharmadrive_noob/web/js/bootstrap.js"></script>
+<script type="text/javascript" src="/ppe_pharmadrive/web/js/bootstrap.js"></script>
 
 </footer>
 </body>
