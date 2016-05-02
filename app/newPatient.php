58d2c018b3c7456fca3119d2d68b73f161a682ea
Julien Coutault <julien.coutault@orange.fr>
2016-05-02 14:19:52 +0200

    Update newPatient.php

--- app/newPatient.php
+++ app/newPatient.php
@@ -7,13 +7,6 @@
 	$ville = (isset($_POST['ville']))?$_POST['ville']:'';
 	$adresse = (isset($_POST['adresse']))?$_POST['adresse']:'';
 
-	if(!numSecuCorrect($secu))
-	{
-		//si le noméro de securité sociale ne correspond pas
-		$_SESSION['alert'] = new Alert('erreur','Numéro de sécurité social incorrect');
-		redirige('/ppe_pharmadrive_noob/patient/nouveau');
-	}
-
 	//on créer un nouveau patient et on le rempli
 	$patient = new Patient();
 	$patient->setNom($nom);
@@ -27,12 +20,12 @@
 	{
 		//on a bien ajouté le patient
 		$_SESSION['alert'] = new Alert('success','Patient ajouté');
-		redirige('/ppe_pharmadrive_noob/patient');
+		redirige('/ppe_pharmadrive/patient');
 	}
 	else
 	{
 		$_SESSION['alert'] = new Alert('danger','Une erreur est survenue');
-		redirige('/ppe_pharmadrive_noob/patient/nouveau');
+		redirige('/ppe_pharmadrive/patient/nouveau');
 	}
 
 	
